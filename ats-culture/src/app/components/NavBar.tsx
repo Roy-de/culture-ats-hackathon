d358581659fb5e0ffb3ed1dc498545bf018ccddd
Roy Murithi <roymuri28@gmail.com>
2024-06-05 13:08:53 +0300

    with image

---
+++ ats-culture/src/app/components/NavBar.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+
+function NavBar() {
+    return (
+        <div className={"w-screen"} >
+            <img src={"/NZ9_0559-1024x731.jpg"} alt={""} className={"w-screen"} style={{width:"100%", height:"100%"}}/>
+        </div>
+    );
+}
+
+export default NavBar;
